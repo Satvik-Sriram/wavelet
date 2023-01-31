6b31401bd099a91a38a48a1191ddad1fcfcb4a2a
SatvikS7 <79161050+SatvikS7@users.noreply.github.com>
2023-01-30 17:06:09 -0800

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    String str = "";
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                if(str.equals("")) {
+                    str = parameters[1];
+                } else {
+                    str = str + "\n" + parameters[1];
+                }
+                return String.format("%s", str);
+            }
+        }
+        return "404 Not Found!";
+        
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println(
+                "Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
